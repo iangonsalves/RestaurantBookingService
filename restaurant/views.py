0abfa29d8d41b192c916a89276d79ed2c5af8ea3
Ian <iangonsalves29@gmail.com>
2025-03-29 15:08:49 +0400

    updated commenting

--- restaurant/views.py
+++ restaurant/views.py
@@ -105,24 +105,3 @@ def bookings(request):
         'available_slots': available_slots,
         'reserved_slots': reserved_slots
     })
-
-
-    #     data = json.load(request)
-    #     exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
-    #         reservation_slot=data['reservation_slot']).exists()
-    #     if exist==False:
-    #         booking = Booking(
-    #             first_name=data['first_name'],
-    #             reservation_date=data['reservation_date'],
-    #             reservation_slot=data['reservation_slot'],
-    #         )
-    #         booking.save()
-    #     else:
-    #         return HttpResponse("{'error':1}", content_type='application/json')
-    
-    # date = request.GET.get('date',datetime.today().date())
-
-    # bookings = Booking.objects.all().filter(reservation_date=date)
-    # booking_json = serializers.serialize('json', bookings)
-
-    # return HttpResponse(booking_json, content_type='application/json')
